2523452f48729286b00aff4fe9aec00078221812
Miquel <miquel.miro@uib.cat>
2025-10-10 11:51:47 +0200

    Millorada solució monedes

--- src/monedes/solucio/agent.py
+++ src/monedes/solucio/agent.py
@@ -9,7 +9,7 @@ SOLUCIO = " XXXC"
 class AgentMoneda(agent.Agent):
     def __init__(self):
         super().__init__(long_memoria=0)
-        self.__oberts = None
+        self.__frontera = None
         self.__tancats = None
         self.__accions = None
 
@@ -17,14 +17,14 @@ class AgentMoneda(agent.Agent):
         print(self._posicio_pintar)
 
     def cerca(self, estat_inicial):
-        self.__oberts = PriorityQueue()
+        self.__frontera = PriorityQueue()
         self.__tancats = set()
 
-        self.__oberts.put((estat_inicial.calc_heuristica(), estat_inicial))
+        self.__frontera.put((estat_inicial.calc_heuristica(), estat_inicial))
 
         actual = None
-        while not self.__oberts.empty():
-            _, actual = self.__oberts.get()
+        while not self.__frontera.empty():
+            _, actual = self.__frontera.get()
             if actual in self.__tancats:
                 continue
 
@@ -34,7 +34,7 @@ class AgentMoneda(agent.Agent):
             estats_fills = actual.genera_fills()
 
             for estat_f in estats_fills:
-                self.__oberts.put((estat_f.calc_heuristica(), estat_f))
+                self.__frontera.put((estat_f.calc_heuristica(), estat_f))
 
             self.__tancats.add(actual)
 
